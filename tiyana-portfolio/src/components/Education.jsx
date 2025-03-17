770f312b7c47620ca15c01a492d71e81ac61e8d6
Tiyana04 <tiyanaludowyke04@gmail.com>
2025-03-17 19:42:51 +0530

    Add to Education

--- tiyana-portfolio/src/components/Education.jsx
+++ tiyana-portfolio/src/components/Education.jsx
@@ -4,17 +4,34 @@ const Education = () => {
   const educationData = [
     {
       degree: 'BSc(Hons) Computer Science',
-      institution: 'Informatics Institue of Technology',
-      location: 'Colombo, Sri Lanka',
+      institution: 'Informatics Institue of Technology | University of Westminster',
+      location: 'UK',
       duration: '2024 - Present',
-      description: 'Specialized in Artificial Intelligence and Machine Learning. Graduated with distinction.'
+      description: 'Pursuing a degree in Computer Science with a focus on software development, web technologies, UI/UX design and database management.',
+      results: 'Year 1: Ongoing'
     },
     {
-      degree: 'Bachelor of Engineering in Computer Science',
-      institution: 'University Name',
-      location: 'City, Country',
-      duration: '2014 - 2018',
-      description: 'Focused on Software Engineering and Data Structures. Graduated with first-class honors.'
+      degree: 'Diploma in Digital Marketing',
+      institution: 'Open University of Sri Lanka',
+      location: 'Nugegoda, Colombo',
+      duration: '2023 - 2024',
+      description: 'Focused on Software Engineering and Data Structures. Graduated with first-class honors.',
+    },
+    {
+      degree: 'Advanced Level - Edexcel',
+      institution: "St. Bridget's Convent",
+      location: 'Colombo 07, Sri Lanka',
+      duration: '2021 - 2023',
+      description: 'Focused on Software Engineering and Data Structures. Graduated with first-class honors.',
+      results: ''
+    },
+    {
+      degree: 'Ordinary Level - Local',
+      institution: "St. Bridget's Convent",
+      location: 'Colombo 07, Sri Lanka',
+      duration: '2020 (2021)',
+      description: 'Focused on Software Engineering and Data Structures. Graduated with first-class honors.',
+      results: ''
     }
   ];
 
@@ -31,6 +48,7 @@ const Education = () => {
               <p className="duration">{item.duration}</p>
               <p className="institution">{item.institution}, {item.location}</p>
               <p>{item.description}</p>
+              <p>{item.results}</p>
             </div>
           ))}
         </div>
--- tiyana-portfolio/src/components/Header.jsx
+++ tiyana-portfolio/src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import { FaGithub, FaLinkedin, FaEnvelope } from 'react-icons/fa';
+import { FaGithub, FaLinkedin, FaEnvelope, FaInstagram } from 'react-icons/fa';
 
 const Header = () => {
   return (
     <header className="header" id="home">
       <div className="header-content">
-        <h1>Your Name</h1>
-        <h2>Senior Software Engineer & Tech Innovator</h2>
+        <h1> Tiyana Ludowyke</h1>
+        <h2> Undergraduate Computer Science Student </h2>
+        
         <div className="social-icons">
           <a href="https://github.com/Tiyana04" className="social-icon" target="_blank" rel="noreferrer">
             <FaGithub size={24} />
@@ -19,7 +20,7 @@ const Header = () => {
           </a>
         </div>
       </div>
-      
+
       <a href="#about" className="scroll-down">
         <svg width="30" height="30" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path d="M7 10L12 15L17 10" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
