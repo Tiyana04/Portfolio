d35969ce503d6e39798ca35eb169880de5309d67
Tiyana04 <tiyanaludowyke04@gmail.com>
2025-03-24 19:22:10 +0530

    Update Header.jsx

--- tiyana-portfolio/src/components/Header.jsx
+++ tiyana-portfolio/src/components/Header.jsx
@@ -76,6 +76,7 @@ const Header = () => {
           <div className="navbar-links">
             <a href="#home">Home</a>
             <a href="#about">About</a>
+            <a href="#education">Education</a>
             <a href="#skills">Skills</a>
             <a href="#projects">Projects</a>
             <a href="#contact">Contact</a>
